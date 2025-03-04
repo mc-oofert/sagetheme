30bd6bed10d56018a4d01418a43e2d39b100d767
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2025-03-04 19:44:48 +0100

    init

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,23 @@
+<?php
+$txt['social_icons'] = 'Social Icons';
+$txt['facebook_url'] = 'Facebook URL';
+$txt['facebook_title'] = 'Facebook';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['twitter_title'] = 'Twitter';
+$txt['youtube_url'] = 'YouTube URL';
+$txt['youtube_title'] = 'Youtube';
+$txt['twitch_url'] = 'Twitch URL';
+$txt['twitch_title'] = 'Twitch';
+$txt['discord_url'] = 'Discord URL';
+$txt['discord_title'] = 'Discord';
+$txt['linkedin_url'] = 'LinkedIn URL';
+$txt['linkedin_title'] = 'LinkedIn';
+$txt['github_url'] = 'GitHub URL';
+$txt['github_title'] = 'GitHub';
+$txt['rss_url'] = 'RSS URL';
+$txt['rss_title'] = 'RSS';
+$txt['social_icons_menu_enabled'] = 'Enable Social Icons';
+$txt['social_icons_desc'] = 'Add https:// to the start of any link used.';
+$txt['custom_forum_width'] = 'Forum Width';
+$txt['custom_forum_width_desc'] = 'Set the forum width. Examples: 980px, 90%, 75%, 1200px.';
+?>
